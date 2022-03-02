04864f8a7406614eaa9f623fcc2af2ae9f4dfe53
Yair Santiago <99760917+unoyira@users.noreply.github.com>
2022-03-02 17:37:02 -0600

    Added: First part practises

---
+++ 4. Testing React with Jest and Testing Library/2-disabled-button-style/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const buttonStyle = (buttonColor, disabled) => ({
+  backgroundColor: disabled ? 'gray' : buttonColor, 
+  color: 'white'
+})
+
+function App() {
+  const [buttonColor, setButtonColor] = useState('red');
+  const [disabled, setDisabled] = useState(false);
+
+  const newColor = buttonColor === 'red'
+    ? 'blue'
+    : 'red';
+
+  const alternateColor = () => setButtonColor(newColor);
+  const handleChecked = ({ target: { checked } }) => setDisabled(checked);
+
+  return (
+    <div className="App">
+      <button
+        style={buttonStyle(buttonColor, disabled)}
+        onClick={alternateColor}
+        disabled={disabled}
+      >
+        Change to {newColor}
+      </button>
+      <input 
+        type="checkbox" 
+        defaultChecked={false}
+        aria-checked={disabled}
+        onChange={handleChecked}
+        id="check-disable-button"
+      />
+    </div>
+  );
+}
+
+export default App;
